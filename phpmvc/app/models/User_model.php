84a11a44eb8a51dd175328523abd6d644952d3d1
widyaprihardiyanti <widyaprihardiyanti33@gmail.com>
2020-06-02 22:31:16 +0700

    project empat

---
+++ phpmvc/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php
+
+class User_model {
+    private $nama = 'Widya';
+
+    public function getUser()
+    {
+        return $this->nama;
+    }
+}
---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="mt-4 container">
+    <h1>About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Widya" width="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun,  saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+</div>
